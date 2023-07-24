a2db11c3208be6c30817c01fd65b7c3f4fe048eb
JonatasTB <101901086+jonatastb@users.noreply.github.com>
2023-07-24 00:33:02 -0300

    :sparkles: feat: Update view html

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,7 +1,6 @@
 <?php
 
 require "model/Model.php";
-require "view/View.php";
 
 class Controller
 {
@@ -9,9 +8,8 @@ class Controller
     public function Index()
     {
         $model = new Model();
-        $view = new View();
-
-        $view->Exibir($model->ListarDados());
+        $dados = $model->ListarDados();
+        include "view/View.php";
     }
 }
 
